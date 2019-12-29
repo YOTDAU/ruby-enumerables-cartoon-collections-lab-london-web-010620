85102ce31fbbbfc81695f1c4cda3702845d93db8
Simon Tucker <sjtucker28@gmail.com>
2019-12-29 22:24:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.map do {|element| element.length < 4}
+  array.map do {|element| if element.length < 4}
   return true 
+else
+  return false 
 end
 
 def find_the_cheese# code an argument here
