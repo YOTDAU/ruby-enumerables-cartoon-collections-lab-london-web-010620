8990006514f50c6a69e310fa1be0253257f266cc
Simon Tucker <sjtucker28@gmail.com>
2019-12-29 22:30:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each_include
 end
 
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
