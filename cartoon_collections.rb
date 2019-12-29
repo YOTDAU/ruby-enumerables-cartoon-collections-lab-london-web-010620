d48d77697e1eccc4d898b317562cf7216b9d66df
Simon Tucker <sjtucker28@gmail.com>
2019-12-29 22:29:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ def long_planeteer_calls(array)
   } 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  
+end
+
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
